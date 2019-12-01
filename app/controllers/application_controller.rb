86de4396e7ef4107a5e5efd4ffc9893fe2884000
Zach  Smith <zdsmith001@gmail.com>
2019-12-01 19:01:51 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,31 +12,37 @@ class ApplicationController < Sinatra::Base
     redirect to '/articles'
   end
 
+  # index
   get '/articles' do
     @articles = Article.all
     erb :index
   end
-
+  
+  # new
   get '/articles/new' do
     @article = Article.new
     erb :new
   end
 
+  # create
   post '/articles' do
     @article = Article.create(params)
     redirect to "/articles/#{ @article.id }"
   end
 
+  # show
   get '/articles/:id' do
     @article = Article.find(params[:id])
     erb :show
   end
 
+  # edit
   get 'articles/:id/edit' do
     @article = Article.find(params[:id])
     erb :edit
   end
 
+  # update
   patch '/articles/:id' do
     @article = Article.find(params[:id])
     @article.update(params[:article])

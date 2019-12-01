e7725bcda9be10e80902b5164ddc1481d45a9eac
Zach  Smith <zdsmith001@gmail.com>
2019-12-01 18:55:20 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -37,7 +37,7 @@ class ApplicationController < Sinatra::Base
     erb :edit
   end
 
-  patch 'articles/:id/edit' do
+  patch 'articles/:id' do
     @article = Article.find(params[:id])
     @article.update(params[:article])
     redirect to "/articles/#{ @article.id }"
